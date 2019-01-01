c489dec0a6c754f50dc931948bf717667ee7475a
ClarkPhan <clarkphan24@gmail.com>
2019-01-01 02:39:55 -0800

    installed react-fontawesome

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import Searchbar from './components/Searchbar/Searchbar';
 import LeagueTrackLogo from './images/LeagueTrackLogo.png';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import './css/App.css';
+library.add(faSpinner);
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="section">
+        <div id="landingPage" className="section">
           <div className="container">
             <figure className="image"><img className="resize" src={LeagueTrackLogo} alt="logo" /></figure>
             <Searchbar />
           </div>
         </div>
-        <div className="footer">
+        <div id="landingPageFooter" className="footer">
           <div className="content has-text-centered">
             <p><strong>LeagueTrack</strong> by <a href="http://clarkphan.com" target="_blank" rel="noopener noreferrer">Clark P</a></p>
           </div>
