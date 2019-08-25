2bd998624a965786750a39a85f7b1efe3ba672d7
Clark Phan <clarkphan24@gmail.com>
2019-08-24 17:16:34 -0700

    Created utils.js for utility functions

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+// Helper function to print data in console
+export const printData = (data) => console.log(JSON.stringify(JSON.parse(data), null, 2));
+
+// Helper function to convert roman numerals into integers
+export const romanToInt = (num) => {
+  const roman = ['I', 'II', 'III', 'IV'];
+  const value = ['1', '2', '3', '4'];
+  for (let i = 0; i < roman.length; i += 1) {
+    if (roman[i] === num) {
+      return value[i].toString();
+    }
+  }
+  return 'error';
+};
