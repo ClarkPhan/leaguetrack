32186768317afd01533f91ffe3534a43ed938d4f
Clark Phan <clarkphan24@gmail.com>
2020-04-07 23:58:13 -0700

    Implement full height landing page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Searchbar from './components/searchbar/Searchbar';
+import Footer from './components/footer/Footer';
 import LeagueTrackLogo from './images/LeagueTrackLogo.png';
 
 import './css/App.css';
@@ -10,19 +11,14 @@ library.add(faSpinner);
 
 const App = () => (
   <div>
-    <div id="landingPage" className="section">
+    <div className="hero is-fullheight">
       <div className="container">
         <figure className="image"><img className="resize" src={LeagueTrackLogo} alt="logo" /></figure>
         <Searchbar />
       </div>
+      <Footer />
     </div>
-    <div id="landingPageFooter" className="footer">
-      <div className="content has-text-centered">
-        <p>
-          <strong>LeagueTrack</strong> by <a href="http://clarkphan.com" target="_blank" rel="noopener noreferrer">Clark P</a>
-        </p>
-      </div>
-    </div>
+
   </div>
 );
 
