2e9baf5944032cc5fcb006ef2f90e43a19b7bff3
Clark Phan <clarkphan24@gmail.com>
2020-04-08 18:10:10 -0700

    Import React Router into App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Searchbar from './components/searchbar/Searchbar';
+import SummonerPage from './components/pages/SummonerPage';
 import Footer from './components/footer/Footer';
 import LeagueTrackLogo from './images/LeagueTrackLogo.png';
-
 import './css/App.css';
 
 library.add(faSpinner);
 
 const App = () => (
-  <div>
-    <div className="hero is-fullheight">
-      <div className="container">
-        <figure className="image"><img className="resize" src={LeagueTrackLogo} alt="logo" /></figure>
-        <Searchbar />
-      </div>
-      <Footer />
-    </div>
-
-  </div>
+  <Router>
+    <Route
+      exact
+      path="/"
+      render={() => (
+        <div className="hero is-fullheight">
+          <div className="container">
+            <figure className="image"><img className="resize" src={LeagueTrackLogo} alt="logo" /></figure>
+            <Searchbar />
+          </div>
+          <Footer />
+        </div>
+      )}
+    />
+    <Route path="/summoner/:username" component={SummonerPage} />
+  </Router>
 );
 
 
