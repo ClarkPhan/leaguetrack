ce3362a7e57e53b525d8298126e2818a434ae2ab
Clark Phan <clarkphan24@gmail.com>
2020-04-09 17:37:04 -0700

    Configure react-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,10 @@ import {
   applyMiddleware,
 } from 'redux';
 
+// Redux persist
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 // Redux middleware
 import ReduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
@@ -12,15 +16,35 @@ import logger from 'redux-logger';
 // Component reducers
 import searchbar from './reducers/searchbar/reducer';
 import modal from './reducers/modal/reducer';
+import summonerProfile from './reducers/summonerProfile/reducer';
 
-const store = createStore(
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig,
   combineReducers({
     searchbar,
     modal,
-  }),
+    summonerProfile,
+  }));
+
+// Redux: Store
+const store = createStore(
+  persistedReducer,
   compose(
     applyMiddleware(ReduxThunk, logger),
+
+    // Redux Debug for Firefox Extension
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
 
-export default store;
+// Middleware: Redux Persist Persister
+const persistor = persistStore(store);
+
+export {
+  store,
+  persistor,
+};
