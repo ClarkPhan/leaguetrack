ff466953828ce1558ea59ad88ba998af90d6dcd4
Clark Phan <clarkphan24@gmail.com>
2020-04-10 19:03:14 -0700

    Create LandingPage.jsx

---
+++ src/components/pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import LandingPageSplash from '../../images/LandingPageSplash.png';
+import Searchbar from '../searchbar/Searchbar';
+import Footer from '../footer/Footer';
+
+export default class LandingPage extends PureComponent {
+  render() {
+    return (
+      <div className="hero is-fullheight">
+        <div className="container">
+          <figure className="image"><img className="resize" src={LandingPageSplash} alt="logo" /></figure>
+          <Searchbar />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
