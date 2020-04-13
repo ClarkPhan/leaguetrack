b2eac6ce40b19a8b1e0256eaf3b4c0b8425b1594
Clark Phan <clarkphan24@gmail.com>
2020-04-13 16:21:56 -0700

    Update reducer.js

--- src/redux/reducers/searchbar/reducer.js
+++ src/redux/reducers/searchbar/reducer.js
@@ -8,14 +8,12 @@ export default function reducer(state = initialState, action) {
   switch (action.type) {
     case ADD_SEARCH: {
       return {
-        ...state,
         searches: [...state.searches, action.payload],
       };
     }
     case DELETE_SEARCH: {
       const filteredSearches = state.searches.filter((search) => (search !== action.payload));
       return {
-        ...state,
         searches: filteredSearches,
       };
     }
